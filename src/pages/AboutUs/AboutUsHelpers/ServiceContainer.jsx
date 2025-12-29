fc421a66a28490df9321c2e1ad70487199f70d4d
Devendar-N <devendar.n@numentica-ui.com>
2025-12-29 15:53:08 +0530

    Changes to be committed: 	modified:   src/pages/AboutUs/AboutUsHelpers/ServiceContainer.jsx

--- src/pages/AboutUs/AboutUsHelpers/ServiceContainer.jsx
+++ src/pages/AboutUs/AboutUsHelpers/ServiceContainer.jsx
@@ -6,17 +6,7 @@ import {
 
 import "../styles/Employee.css";
 
-export const ServiceContainer = () => {
-  return (
-    <div className="service-container">
-        <Service ServiceImage ={DeliveryIcon} ServiceTitle= "FREE AND FAST DELIVERY" ServiceContent= "Free delivery for all order over $140" />
-        <Service ServiceImage ={CustomerSupport} ServiceTitle= "24/7 CUSTOMER SERVICE" ServiceContent= "Friendly 24/7 customer support" />
-        <Service ServiceImage ={Guarantee} ServiceTitle= "MONEY BACK GUARENTEE" ServiceContent= "We return money within 30 days" />
-    </div>
-  )
-}
-
-
+//Service card
 export const Service = ({
     ServiceImage,
     ServiceTitle,
@@ -32,3 +22,14 @@ export const Service = ({
         </div>
   )
 }
+//Accessing cards
+export const ServiceContainer = () => {
+  return (
+    <div className="service-container">
+        <Service ServiceImage ={DeliveryIcon} ServiceTitle= "FREE AND FAST DELIVERY" ServiceContent= "Free delivery for all order over $140" />
+        <Service ServiceImage ={CustomerSupport} ServiceTitle= "24/7 CUSTOMER SERVICE" ServiceContent= "Friendly 24/7 customer support" />
+        <Service ServiceImage ={Guarantee} ServiceTitle= "MONEY BACK GUARENTEE" ServiceContent= "We return money within 30 days" />
+    </div>
+  )
+}
+
